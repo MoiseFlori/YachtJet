c58f622f562c1b59250eb94bdc6e3ece41a977f5
MoiseFlori <moiseflori@outlook.com>
2024-10-18 13:09:20 +0300

    update mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,15 @@
-
 const modalOpen = document.getElementsByClassName("modal-open")[0];
 modalOpen.addEventListener('click', openModal, false);
+
 function openModal() {
-    document.getElementById("mobile-menu").className = "mobile-menu-container is-open";
+    document.getElementById("mobile-menu").classList.add("is-open"); // Adaugă doar clasa 'is-open'
 }
 
 const modalClose = document.getElementsByClassName("modal-close");
 for (let i = 0; i < modalClose.length; i++) {
     modalClose[i].addEventListener('click', closeModal, false);
 }
-function closeModal() {
-    document.getElementById("mobile-menu").className = "mobile-menu-container";
-}
 
+function closeModal() {
+    document.getElementById("mobile-menu").classList.remove("is-open"); // Elimină doar clasa 'is-open'
+}
